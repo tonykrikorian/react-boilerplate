e6d7b9d93684a76cfcc1ab42d2f571304bb7057d
Tony <tkrikoriam@achs.cl>
2020-05-19 08:03:14 -0400

    Config enviroments

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import MainContainer from '../containers/MainContainer';
+import MainContainer from '../components/containers/MainContainer';
 
 
 function App() {
