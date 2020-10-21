e772ef8a6b21067fc6d3a4d096ff07b4525e8b4f
tkrikoriam <tkrikoriam@achs.cl>
2020-10-21 15:22:26 -0300

    Add Mainpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import MainContainer from "./containers/MainContainer";
+import Index from "./pages/index";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={MainContainer} />
+        <Route exact path="/" component={Index} />
       </Switch>
     </BrowserRouter>
   );
